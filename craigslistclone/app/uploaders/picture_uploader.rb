6cd20e0714d15aef6d8dc8968059d942761a678a
Patrick Putman <putman.patrick@gmail.com>
2011-07-15 18:00:10 -0500

    Added a default no image on the post index

--- craigslistclone/app/uploaders/picture_uploader.rb
+++ craigslistclone/app/uploaders/picture_uploader.rb
@@ -21,9 +21,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+   "fallback/" + [version_name, "no_image.gif"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
